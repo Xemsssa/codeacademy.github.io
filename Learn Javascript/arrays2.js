245401eb247f529fa8d0a32fcd2e4e12946f4084
Xemsssa <Xemsss@gmail.com>
2017-09-14 19:08:21 +0300

    Learn Javascript

---
+++ Learn Javascript/arrays2.js
@@ -0,0 +1,13 @@
+var bucketList = [
+  'first', 
+  'second',
+  'third'
+];
+
+bucketList.push('four','five');
+bucketList.pop();
+console.log(bucketList);
+
+var listItem = bucketList[3];
+console.log(listItem);
+console.log(bucketList.length);
