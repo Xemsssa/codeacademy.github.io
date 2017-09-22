b26e32a4a41646ac2c943b9d9666f8e2f6e21f3e
Xemsssa <Xemsss@gmail.com>
2017-09-22 20:51:00 +0300

    Into JS Requests

---
+++ Introduction To JavaScript/Requests/ajax.js
@@ -0,0 +1,13 @@
+$.ajax({
+  url: 'https://api-to-call.com/endpoint',
+  type: 'GET',
+  dataType: 'json',
+  
+  success(response) {
+    console.log(response);
+  },
+  
+  error(jqXHR, status, errorThrown) {
+  	console.log(jqXHR);
+	}
+});
