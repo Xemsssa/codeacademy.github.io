c962f331d895faf8d9c009aa0771b3be31502479
Xemsssa <Xemsss@gmail.com>
2017-09-26 21:13:30 +0300

    Continue java

---
+++ java/Ternary.java
@@ -0,0 +1,11 @@
+public class Ternary {
+	public static void main(String[] args) {
+		
+		int fuelLevel = 3;
+
+		char canDrive = (fuelLevel > 0 ) ? 'Y' : 'N';
+		System.out.println(canDrive);
+
+	}
+}
+
