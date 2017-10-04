cd0a4aeb955daedfee335191d1b55ebb3fb1b607
Xemsssa <Xemsss@gmail.com>
2017-10-04 19:45:03 +0300

    into java

---
+++ java/Data Structures/Restaurant.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+
+public class Restaurant {
+	public static void main(String[] args) {
+		HashMap<String, Integer> restaurantMenu = new 
+			HashMap<String, Integer>();
+	}
+}
